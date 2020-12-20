3be3de7c76beb2b68e441fe73ad9d1ec60316f41
Jacob Berg <coffeesnakes@gmail.com>
2020-12-20 15:41:59 -0800

    working on server/db connection

---
+++ database/index.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+
+const dbconnect = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '3327',
+  database: 'movielistsql'
+});
+dbconnect.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`connected to database`)
+  }
+})
+
+
+module.exports = dbconnect;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const port = 4004;
 const app = express();
+const db = require ('../database/index.js');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-  res.send(`we're live on ${port}`)
-});
+// app.get('/', (req, res) => {
+//   res.send(`we're live on ${port}`)
+// });
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.get('/movies', (req, res) => {
+  db.query('SELECT * FROM movies', (err, data, field) => {
+    if (err) {
+      console.log(err)
+      res.sendStatus(404)
+    } else {
+      console.log(data)
+      res.send(rows);
+    }
+  })
+})
 
 app.listen(port, console.log(`listening on: ${port}`));
