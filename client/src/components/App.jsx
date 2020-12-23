8af299bb3a3a61fdcdcba2bf63dea443bd69580c
Jacob Berg <coffeesnakes@gmail.com>
2020-12-22 23:04:56 -0800

    working on handling user input

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,41 +1,47 @@
-import React from 'react';
-import axios from 'axios';
-import Header from './Header.jsx';
-import MovieList from './MovieList.jsx';
-import MovieForm from './MovieForm.jsx';
+import React from "react";
+import axios from "axios";
+import Header from "./Header.jsx";
+import MovieList from "./MovieList.jsx";
+import MovieForm from "./MovieForm.jsx";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: []
+      movies: [],
     };
     this.getMovies = this.getMovies.bind(this);
-
-// bindings go here, attatch the word this to state (be mindful of lifting state up)
+    this.addMovie = this.addMovie.bind(this);
+    // bindings go here, attatch the word this to state (be mindful of lifting state up)
   }
 
-componentDidMount() {
-  this.getMovies()
-}
+  componentDidMount() {
+    this.getMovies();
+  }
 
   getMovies() {
-    axios
-      .get("/movies")
+    axios.get("/movies")
       .then((res) => {
         const { data } = res;
-        this.setState({ movies: data });
-      })
-      .catch((err) => console.log(err));
+        this.setState({ movies: data });})
+      .catch((err) => console.log(err));}
+
+  addMovie(movie) {
+    console.log(movie)
+    axios.post("/movies", movie)
+
+    .then(() => {
+      this.getMovies()
+    })
+    .catch((err)=> console.log(err));
   }
 
   render() {
-// console.log(this.state.movies)
     return (
       <div>
         <Header />
-        < MovieForm />
-        < MovieList movies={this.state.movies} />
+        <MovieForm submitHandler={this.addMovie} />
+        <MovieList movies={this.state.movies} />
       </div>
     );
   }
